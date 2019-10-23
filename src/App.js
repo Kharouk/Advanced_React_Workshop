602a07980d734df455abd07ffd7287c0f1ede1ec
Alex Kharouk <akharoukpp@gmail.com>
2019-10-23 10:34:05 +0100

    refactor toggle

--- src/App.js
+++ src/App.js
@@ -18,15 +18,16 @@ const Counter = () => {
 
 function App() {
   const [counterVisibility, setCounterVis] = useState(false);
-
   const toggleVisibility = () => setCounterVis(!counterVisibility);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Advanced React With Kitze</h1>
+        <button onClick={toggleVisibility}>
+          {counterVisibility ? "Hide Counter" : "Show Counter"}
+        </button>
         {counterVisibility && <Counter />}
-        <button onClick={toggleVisibility}>Toggle Counter</button>
       </header>
     </div>
   );
