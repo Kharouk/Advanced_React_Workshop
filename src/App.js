9c30493092a3fd73b931aaf9eb592c19e5d4d5cc
Alex Kharouk <akharoukpp@gmail.com>
2019-10-23 10:32:04 +0100

    create counter and start toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const Counter = () => {
+  const initialNumber = 0;
+  const [number, setNumber] = useState(initialNumber);
+  const incrementCounter = () => setNumber(number + 1);
+  const decrementCounter = () => setNumber(number - 1);
+
+  return (
+    <div>
+      <h2>Counter is at {number}</h2>
+      <button onClick={incrementCounter}>Increment by 1</button>
+      <button onClick={decrementCounter}>Decrement by 1</button>
+    </div>
+  );
+};
 
 function App() {
+  const [counterVisibility, setCounterVis] = useState(false);
+
+  const toggleVisibility = () => setCounterVis(!counterVisibility);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Advanced React With Kitze</h1>
+        {counterVisibility && <Counter />}
+        <button onClick={toggleVisibility}>Toggle Counter</button>
       </header>
     </div>
   );
